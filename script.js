4b46ffabb298eb0258b5573a2b837e0e96a95088
davi-barross <davibarrosgonzaga@gmail.com>
2024-02-14 13:05:34 -0300

    segundo projeto

---
+++ script.js
@@ -0,0 +1,25 @@
+let nome = "davi"
+let xp =  1000
+let elo = ""
+
+if (xp <= elo){
+    elo  = "ferro"
+}else if (xp <= 2000){
+    elo  = "bronze"
+}
+else if (xp <= 5000){
+    elo  = "ouro"
+} else if (xp <= 7000)
+elo  = "platina" 
+else if (xp <= 8000) {
+    elo  = "ascendente"
+
+} else if (xp <= 9000){
+    elo  = "imortal"
+} else if (xp>= 10000) {
+    elo = radiante
+}
+
+
+console.log("O Herói de nome **{nome}** está no nível e elo de **elo** }")
+
